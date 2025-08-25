c073cd64c696029af23a1c98172b42eef36ab5c3
ItzVarad <70215793+ItzVarad@users.noreply.github.com>
2025-08-25 22:33:13 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Number of occurrence/number-of-occurrence.java
+++ Difficulty: Easy/Number of occurrence/number-of-occurrence.java
@@ -1,32 +1,3 @@
-//{ Driver Code Starts
-// Initial Template for Java
-
-import java.io.*;
-import java.util.*;
-
-class GFG {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int tc = Integer.parseInt(br.readLine().trim());
-
-        while (tc-- > 0) {
-
-            String[] str = br.readLine().trim().split(" ");
-            int[] a = new int[str.length];
-            for (int i = 0; i < str.length; i++) {
-                a[i] = Integer.parseInt(str[i]);
-            }
-            String[] nk = br.readLine().trim().split(" ");
-            int k = Integer.parseInt(nk[0]);
-            Solution sln = new Solution();
-            int ans = sln.countFreq(a, k);
-
-            System.out.println(ans);
-            System.out.println("~");
-        }
-    }
-}
-// } Driver Code Ends
 
 
 class Solution {
@@ -35,12 +6,13 @@ class Solution {
         int f =0,l=0;
         f = binarySearch(arr,target,true);
         l = binarySearch(arr,target,false);
-        if(f == -1 || l == -1){
-            if(f == -1 && l == -1){
-                return 0;
-            } else if(f == -1) f = 0;
-            else if(l == -1) l = 0;
-        }
+        
+        if(f == -1 && l == -1){
+            return 0;
+        } 
+        if(f == -1) f = 0;
+        if(l == -1) l = 0;
+        
         return l - f + 1; 
     }
     private int binarySearch(int[] nums, int target, boolean isFindLeft){
